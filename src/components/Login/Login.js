f4e2b6a1244c8cf6361f4b7b44a2a2ba802578b1
huytdn <saolailatoivay991@gmail.com>
2025-08-18 21:56:59 +0700

    login scss

---
+++ src/components/Login/Login.js
@@ -0,0 +1,43 @@
+import "./Login.scss";
+
+const Login = (props) => {
+  return (
+    <div className="login-container ">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Huy TDN AKA</div>
+            <div className="detail">
+              AKA helps you connect and share with the people in your life .
+            </div>
+          </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none">Huy TDN AKA</div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number "
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">
+              <a href="#" className="forgot-password">
+                Forgot your password
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success"> Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
